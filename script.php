792b0faf80de7bb88d065c652b5f2257cd5f9494
Joelo <salazarjoelo@gmail.com>
2025-05-10 17:05:36 -0500

    Update script.php

--- script.php
+++ script.php
@@ -12,12 +12,60 @@ defined('_JEXEC') or die;
 use Joomla\CMS\Installer\InstallerScript;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
+// Añade más 'use' si los necesitas para lógica específica
 
+/**
+ * Installation script for the LineaDeTiempo component.
+ */
 class Com_LineadetiempoInstallerScript extends InstallerScript
 {
-    public string $minimumJoomla = '5.0.0';
-    public string $minimumPhp = '8.1.0';
+    /**
+     * Minimum Joomla version requirement for this component.
+     * @var string
+     */
+    public $minimumJoomla = '5.0.0'; // Tipo 'string' ELIMINADO
 
+    /**
+     * Minimum PHP version requirement for this component.
+     * @var string
+     */
+    public $minimumPhp = '8.1.0';    // Tipo 'string' ELIMINADO
+
+    /**
+     * Method to run before an install/update/uninstall method.
+     *
+     * @param   string  $type    The type of change (install, update or discover_install).
+     * @param   InstallerAdapter $parent  The class calling this method.
+     *
+     * @return  bool    True on success.
+     */
+    public function preflight($type, $parent): bool
+    {
+        // $app = Factory::getApplication();
+        // $app->enqueueMessage('Preflight check for ' . $type, 'message');
+        // Aquí puedes añadir validaciones (ej. versión de PHP, otras extensiones)
+        
+        // Ejemplo de verificación de versión mínima de PHP más explícita si es necesario
+        // if (!version_compare(PHP_VERSION, $this->minimumPhp, '>=')) {
+        //     Factory::getApplication()->enqueueMessage(Text::sprintf('JLIB_INSTALLER_MINIMUM_PHP_VERSION_REQUIRED', $this->minimumPhp), 'error');
+        //     return false;
+        // }
+        // Similar para minimumJoomla si es necesario
+        // if (!version_compare(JVERSION, $this->minimumJoomla, '>=')) {
+        //    Factory::getApplication()->enqueueMessage(Text::sprintf('COM_LINEADETIEMPO_ERROR_JOOMLA_VERSION_TOO_LOW', $this->minimumJoomla), 'error');
+        //    return false;
+        // }
+
+        return true;
+    }
+
+    /**
+     * Method to install the component.
+     *
+     * @param   InstallerAdapter $parent  The class calling this method.
+     *
+     * @return  bool    True on success.
+     */
     public function install($parent): bool
     {
         Factory::getApplication()->enqueueMessage(Text::_('COM_LINEADETIEMPO_INSTALL_SUCCESS_MSG'), 'message');
@@ -25,30 +73,46 @@ class Com_LineadetiempoInstallerScript extends InstallerScript
         return true;
     }
 
-    public function uninstall($parent): bool
-    {
-        Factory::getApplication()->enqueueMessage(Text::_('COM_LINEADETIEMPO_UNINSTALL_SUCCESS_MSG'), 'message');
-        return true;
-    }
-
+    /**
+     * Method to update the component.
+     *
+     * @param   InstallerAdapter $parent  The class calling this method.
+     *
+     * @return  bool    True on success.
+     */
     public function update($parent): bool
     {
         Factory::getApplication()->enqueueMessage(Text::sprintf('COM_LINEADETIEMPO_UPDATE_SUCCESS_MSG', $parent->get('manifest')->version), 'message');
+        // Lógica de actualización
         return true;
     }
 
-    public function preflight($type, $parent): bool
+    /**
+     * Method to uninstall the component.
+     *
+     * @param   InstallerAdapter $parent  The class calling this method.
+     *
+     * @return  bool    True on success.
+     */
+    public function uninstall($parent): bool
     {
-        // $phpVersion = $parent->get('manifest')->php_minimum ?? $this->minimumPhp;
-        // if (!version_compare(PHP_VERSION, $phpVersion, '>=')) {
-        //     Factory::getApplication()->enqueueMessage(Text::sprintf('JLIB_INSTALLER_MINIMUM_PHP_VERSION_REQUIRED', $phpVersion), 'error');
-        //     return false;
-        // }
+        Factory::getApplication()->enqueueMessage(Text::_('COM_LINEADETIEMPO_UNINSTALL_SUCCESS_MSG'), 'message');
+        // Lógica de desinstalación (ej. borrar carpetas o archivos no eliminados por el manifiesto)
         return true;
     }
 
+    /**
+     * Method to run after an install/update/uninstall method.
+     *
+     * @param   string  $type    The type of change (install, update or discover_install).
+     * @param   InstallerAdapter $parent  The class calling this method.
+     *
+     * @return  void
+     */
     public function postflight($type, $parent): void
     {
-        // Acciones post-instalación/actualización si son necesarias
+        // $app = Factory::getApplication();
+        // $app->enqueueMessage('Postflight action for ' . $type, 'message');
+        // Acciones después de la instalación/actualización, como limpiar caché específica.
     }
 }
